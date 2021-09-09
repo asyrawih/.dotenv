6f9a9121f2cc1caf9809ea44333faf6c246fdc42
hananloser <hasyrawi@gmail.com>
2021-09-09 18:24:01 +0800

    resetup

--- init.lua
+++ init.lua
@@ -11,5 +11,15 @@ for i = 1, #chad_modules, 1 do
 end
 
 require("mappings").misc()
-require("go").setup()
+require("go").setup({
+  goimport = 'gopls', -- if set to 'gopls' will use golsp format
+  gofmt = 'gopls', -- if set to gopls will use golsp format
+  max_line_len = 120,
+  tag_transform = false,
+  test_dir = '',
+  comment_placeholder = ' ?  ',
+  lsp_gofumpt = true, -- true: set default gofmt in gopls format to gofumpt
+  lsp_on_attach = true, -- use on_attach from go.nvim
+  dap_debug = true,
+})
 
